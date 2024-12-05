d9ade99371af9e84ce1a3652ae0b132895dffa6c
Ma-n-a-v <manupatel3345@gmail.com>
2024-12-04 23:57:56 -0600

    final commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Java, JavaScript, NodeJS, ExpressJS and full Stack content these days.
+    Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/Ma-n-a-v" target="_blank">Github</a>!`,
+  "cs-degree": `This is the space for my CS degree. I will hung it on the wall. I will be graduating from The University of Winnipeg, 
+  Computer Science at the University of Winnipeg provides a dynamic and inclusive learning environment, offering courses in programming, algorithms, data structures, and specialized areas like artificial intelligence and cybersecurity.`,
+  "sofa-table": `Ah, that's my sofa where I kick back, unwind, and indulge in some Netflix bliss. 
+  It's the ultimate cozy corner, perfect for unwinding and binge-watching my favorite shows. `,
+  tv: `That's my TV. Recently, I've been fascinated by the captivating science explorations of Veritasium, where Derek Muller dives into mind-bending topics and uncovers the mysteries of the natural world. 
+  It's like having a scientific adventure right in my living room! `,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="" target="_blank">Check it out?</a>
+  Contact me at manupatel3345@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about my projects
+  by checking out <a href="https://github.com/Ma-n-a-v" target="_blank">here</a>.`,
+  library: `There are stacks of programming books lining my shelves, including academic course materials and countless technical volumes. It's like a library in here, though I'll admit, I've probably only scratched the surface of what's inside. 
+  Who else can relate to the allure of collecting programming wisdom without quite finishing the last chapter?`,
+  exit: `If you want to exit Manav's portfolio, just close the tab.`,
+};
